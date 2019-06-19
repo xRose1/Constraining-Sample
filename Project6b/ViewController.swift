0789eb090d69734ec9f02b5338d3914e120dfa94
Sanjeev Bedasee <ibedasee@gmail.com>
2019-06-19 08:21:11 -0400

    Initial Commit

---
+++ Project6b/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project6b
+//
+//  Created by Mac on 6/19/19.
+//  Copyright © 2019 Sbedx4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
